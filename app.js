ee80229d92deacd768f30a6af656c43025bcf49a
aldoso <alexdobras@gmail.com>
2017-10-02 08:53:59 +0300

    add / delete in database and interface

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const todoController = require('./controllers/todoController')
-const mongoose = require('mongoose');
+
 
 const app = express()
 
@@ -9,15 +9,6 @@ console.log('This app runs on http://localhost:4000')
 
 app.set('view engine', 'ejs') //set template engine
 app.use(express.static('./public/')) //static files
-
-
-// Connect to the database (mlab.com)
-mongoose.connect('mongodb://test:test@ds159024.mlab.com:59024/aldoso-todo')
-
-// Create a schema - this is like a blueprint
-const todoSchema = new mongoose.Schema({
-  item: String
-})
 
 
 //fire controllers !!!!
