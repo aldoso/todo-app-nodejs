1bbbb90071e74b31bc3e622d4ef28fa3679b0ef1
aldoso <alexdobras@gmail.com>
2017-10-02 08:12:14 +0300

    add form functionality

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ app.listen(process.env.PORT || 4000)
 console.log('This app runs on http://localhost:4000')
 
 app.set('view engine', 'ejs') //set template engine
-app.use(express.static('./public')) //static files
+app.use(express.static('./public/')) //static files
 
-// Connect to the database (mlab.com)
-mongoose.connect('mongodb://test:test@ds159024.mlab.com:59024/aldoso-todo')
+// // Connect to the database (mlab.com)
+// mongoose.connect('mongodb://test:test@ds159024.mlab.com:59024/aldoso-todo')
+//
+// // Create a schema - this is like a blueprint
+// const todoSchema = new mongoose.Schema({
+//   item: String
+// })
 
-// Create a schema - this is like a blueprint
-const todoSchema = new mongoose.Schema({
-  item: String
-})
-
-//fire controllers
+//fire controllers !!!!
 todoController(app)
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,19 +1,23 @@
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'take out the garbage'}]
 
 module.exports = function(app){
-    app.get('/', function(req, res){
+
+  app.get('/', function(req, res){
     res.render('todo', {todos: data})
   })
 
   app.post('/', urlencodedParser, function(req, res){
-    data.push(req.body)
-    response.json(data)
+    data.unshift(req.body)
+    res.json(data)
   })
 
-  app.delete('/', function(req, res){
-    res.send('it works')
+  app.delete('/:item', function(req, res){
+    data = data.filter(function(todo){
+      return todo.item.replace(/ /g, "-") !== req.params.item
+    })
+    res.json(data)
   })
 }
