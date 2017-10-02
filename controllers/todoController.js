1819063f8232688444ca9f350fecec664f043894
aldoso <alexdobras@gmail.com>
2017-10-02 06:16:09 +0300

    add files

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,16 @@
+const bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'take out the garbage'}]
+
 module.exports = function(app){
-  app.get('/', function(req, res){
-    res.send('it works')
+    app.get('/', function(req, res){
+    res.render('todo', {todos: data})
   })
 
-  app.post('/', function(req, res){
-    res.send('it works')
+  app.post('/', urlencodedParser, function(req, res){
+    data.push(req.body)
+    response.json(data)
   })
 
   app.delete('/', function(req, res){
