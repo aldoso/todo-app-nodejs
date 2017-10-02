2390c4d12662f03ffb657c03f222cc55a4590b0b
aldoso <alexdobras@gmail.com>
2017-10-02 08:16:34 +0300

    mLab mongodb

--- app.js
+++ app.js
@@ -10,5 +10,15 @@ console.log('This app runs on http://localhost:4000')
 app.set('view engine', 'ejs') //set template engine
 app.use(express.static('./public/')) //static files
 
+
+// Connect to the database (mlab.com)
+mongoose.connect('mongodb://test:test@ds159024.mlab.com:59024/aldoso-todo')
+
+// Create a schema - this is like a blueprint
+const todoSchema = new mongoose.Schema({
+  item: String
+})
+
+
 //fire controllers !!!!
 todoController(app)
